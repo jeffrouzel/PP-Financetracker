02254bc52ca4e090ed139ac0a9062895dac21840
JeffBat-og <jabatog@up.edu.ph>
2024-07-12 11:46:03 +0800

    Removed a basis code from another project

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,6 @@
 <script setup>
 import { ref } from 'vue';
 
-const users = ref([
-    { id: 1, companyname: "Central Luzon College of Science and Technology" },
-    { id: 2, companyname: "STI College" },
-    { id: 3, companyname: "College of Maritime Leadership Innovation" },
-    { id: 4, companyname: "PCI Tech Center Inc." },
-    { id: 5, companyname: "Concentrix" },
-    { id: 6, companyname: "HySeas III"},
-    { id: 7, companyname: "Kongsberg" },
-    { id: 8, companyname: "Maritime Merchant Marine Academy Graduate School" },
-    { id: 9, companyname: "Hukbong Dagat Philippines" },
-    { id: 10, companyname: "Inter-Global College Foundation Inc." },
-    { id: 11, companyname: "Baliwag Maritime Academy" },
-]);
 </script>
 
 
